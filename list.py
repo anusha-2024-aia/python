6905767c0537675ac22d94512321e9dedc046bd8
anusha-2024-aia <anushaanusha6500@gmail.com>
2025-07-27 19:03:48 +0530

    second initialize

--- list.py
+++ list.py
@@ -11,4 +11,33 @@ print("\n")
 nested_list=[[1,2],[3,4]]
 print(nested_list[0][1])
 
+print("\n")
+print("print all items in a list:")
+lis=[1,2,3,4,5]
+for item in lis:
+    print(item)
+
+print("\n")
+print("The sum of all numbers in a list:")
+list=[2,3,4,9,8]
+sum=0
+for num in list:
+    sum+=num
+print(sum)
+print("\n")
+print("Count how many times an item appears:")
+my_list=[1,3,4,5,6,7,8,5,6,7]
+count=my_list.count(5)
+print(count)
+print("\n")
+print("remove duplicates froma list:")
+list=[1,1,2,2,3,4,5,5]
+unique=""
+for num in list:
+    if num not in unique:
+        unique.append(num)
+print(unique)
+print("\n")
+largest_num=[15,27,9,10,55]
+print(max(largest_num))                
 
