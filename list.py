da20f5995b8921865901a178dc1470920c9bb78a
anusha-2024-aia <anushaanusha6500@gmail.com>
2025-07-27 18:07:32 +0530

    First initial commit

---
+++ list.py
@@ -0,0 +1,14 @@
+my_list=[10,20,30,40,50]
+print(my_list[0])
+print(my_list[::-1])
+print(my_list.remove[0])
+print(my_list)
+print("\n")
+print("squares of list:")
+squares=[x*x for x in range(5)]
+print(squares)
+print("\n")
+nested_list=[[1,2],[3,4]]
+print(nested_list[0][1])
+
+
